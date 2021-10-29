f952e2d94d634cb0ea718512a4ea726696491a65
Margaret <mrobinson526@gmail.com>
2021-10-29 16:15:39 -0500

    second commit

--- server/server.js
+++ server/server.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const bodyParser = require("body-parser");
 let app = express();
-/*app.get('/', (req,res) => {
+app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 app.get('/css/styles.css', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/css/styles.css'));
 })
-}); */
-
+}); 
 app.use((req, res, next) => {
-    console.log(req.originalUrl);
+    fs.appendFileSync('log.txt', `${req.url}\n`);
     next();
 })
 app.use(express.static(path.join(__dirname, '../public'))); 
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.post("contact-form", (req, res) => {
+    console.log(req.body.name);
+    console.log(req.body.email);
+    res.send('Thank you for sending your contact form');
+}) 
+
+
 
 app.listen(3000);
